a4999d628d5a00f253e140d5995c5ee75c633576
lcz <619497669@qq.com>
2017-11-24 20:05:35 +0800

    更新

---
+++ app/src/main/java/com/example/graduationproject/adapter/AuthorVideoAdapter.java
@@ -0,0 +1,10 @@
+package com.example.graduationproject.adapter;
+
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Administrator on 2017/11/23 0023.
+ */
+
+public class AuthorVideoAdapter extends BaseAdapter {
+}
